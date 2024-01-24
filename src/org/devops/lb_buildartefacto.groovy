f3845798c7dce64de9c1d2caf4c2027d12f551b0
CristianBetancourthAvila <148814182+CristianBetancourthAvila@users.noreply.github.com>
2024-01-24 15:16:22 -0500

    Update lb_buildartefacto.groovy

--- src/org/devops/lb_buildartefacto.groovy
+++ src/org/devops/lb_buildartefacto.groovy
@@ -1,7 +1,7 @@
 package org.devops
 
 def clone(Map params){
-    git branch: "${env.GIT_BRANCHS}", url: "${env.GIT_URLS}"
+    git branch: "${env.GIT_BRANCH_1}", url: "${env.GIT_URL_1}"
 }
 
 def install(){
