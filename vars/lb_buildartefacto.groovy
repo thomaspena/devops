2b294376f69aa1d5d47b45667b059599355a62db
CristianBetancourthAvila <cristianbetancourth@periferia-it.com>
2024-01-24 09:15:17 -0500

    First commit

---
+++ vars/lb_buildartefacto.groovy
@@ -0,0 +1,10 @@
+package org.devops
+
+def clone(Map params){
+    def branch_name = "${env.GIT_BRANCH}".split('/')[1]
+    git branch: "${branch_name}", url: "${params.scmUrl}"
+}
+
+def install(){
+    sh 'npm install' 
+}
