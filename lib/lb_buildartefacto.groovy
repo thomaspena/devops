8651def92aec38afe9b9e50d83daa7265a54a348
CristianBetancourthAvila <148814182+CristianBetancourthAvila@users.noreply.github.com>
2024-01-24 09:57:43 -0500

    Update lb_buildartefacto.groovy

--- lib/lb_buildartefacto.groovy
+++ lib/lb_buildartefacto.groovy
@@ -1,1 +1,11 @@
+package org.devops
 
+def clone(Map params){
+    def branch_name = "feature"
+    def url = "https://github.com/CristianBetancourthAvila/react-test.git"
+    git branch: "${branch_name}", url: "${url}"
+}
+
+def install(){
+    sh 'npm install' 
+}
