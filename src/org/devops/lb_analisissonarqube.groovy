6c06cc3df9d09c74fbc4e977f1175dc486658484
CristianBetancourthAvila <148814182+CristianBetancourthAvila@users.noreply.github.com>
2024-01-24 11:40:51 -0500

    Update lb_analisissonarqube.groovy

--- src/org/devops/lb_analisissonarqube.groovy
+++ src/org/devops/lb_analisissonarqube.groovy
@@ -1,1 +1,15 @@
+package org.devops
 
+def analisys(){
+    def scannerHome = tool 'sonar-scanner'
+    withSonarQubeEnv('sonar-scanner') {
+        sh "${scannerHome}/bin/sonar-scanner -X \
+            -Dsonar.projectKey=react-test \
+            -Dsonar.projectName=react-test \
+            -Dsonar.sources=src \
+            -Dsonar.tests=src/__test__ \
+            -Dsonar.exclusions='**/*.test.js' \
+            -Dsonar.testExecutionReportPaths=./test-report.xml \
+            -Dsonar.javascript.lcov.reportPaths=./coverage/lcov.info"
+    }  
+}
