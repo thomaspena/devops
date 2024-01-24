6adf188f9787cad26ee1a66ddf6f53ce94c5a471
CristianBetancourthAvila <148814182+CristianBetancourthAvila@users.noreply.github.com>
2024-01-24 12:08:14 -0500

    Update lb_buildartefacto.groovy

--- src/org/devops/lb_buildartefacto.groovy
+++ src/org/devops/lb_buildartefacto.groovy
@@ -2,9 +2,8 @@ package org.devops
 
 def clone(){
     def branch_name = "master"
-    //def url = "https://github.com/CristianBetancourthAvila/react-test.git"
-    
-    git branch: "${branch_name}", url: "${env.GIT_URL}"
+    def url = "https://github.com/CristianBetancourthAvila/react-test.git"
+    git branch: "${branch_name}", url: "${url}"
 }
 
 def install(){
