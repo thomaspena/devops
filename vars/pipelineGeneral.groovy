85eaada733e7ea129fe06ad31de6b8368825645d
CristianBetancourthAvila <cristianbetancourth@periferia-it.com>
2024-01-30 15:48:24 -0500

    Primeros cambios en develop

---
+++ vars/pipelineGeneral.groovy
@@ -0,0 +1,48 @@
+def call(){
+
+    pipeline{
+
+        agent any
+
+        tools{
+            nodejs 'NodeJS18'
+        }
+        
+        environment{
+            projectName = "${env.GIT_URL_1}".replaceAll('.+/(.+)\\.git', '$1')toLowerCase()
+        }
+
+        stages{
+            stage('Fase 2: Construcción de imagen en Docker Desktop') {
+                steps {
+                    script {
+                        def buildimage = new org.devops.lb_buildimagen()
+                        buildimage.buildImageDocker("${projectName}")
+                    }
+                }
+            }
+
+            /*stage('Fase 1: Proceso de construcción') {
+                steps {
+                    script {
+                        def cloneapp = new org.devops.lb_buildartefacto()
+                        cloneapp.clone()
+                        def buildapp = new org.devops.lb_buildartefacto()
+                        buildapp.install()
+                    }
+                }
+            }
+
+            stage('Fase 1: Análisis de Sonarqube'){
+                steps{
+                    script{
+                        def test = new org.devops.lb_analisissonarqube()
+                        test.runTest()
+                        def analisysSonarqube = new org.devops.lb_analisissonarqube()
+                        analisysSonarqube.analisys("${projectName}")
+                    }
+                }
+            }*/
+        }
+    }
+}
