084287ed1d0f5d039f08ad55e43b3054468cc05c
CristianBetancourthAvila <148814182+CristianBetancourthAvila@users.noreply.github.com>
2024-01-31 08:29:52 -0500

    Update lb_buildimagen.groovy

--- src/org/devops/lb_buildimagen.groovy
+++ src/org/devops/lb_buildimagen.groovy
@@ -1,5 +1,5 @@
 package org.devops
 
 def buildImageDocker(projectGitName){
-    sh "docker run -v /var/run/docker.sock:/var/run/docker.sock -v ${pwd()}:${pwd()} -w ${pwd()} ${projectGitName} docker build -t ${projectGitName} ."
+    sh "docker run -v /var/run/docker.sock:/var/run/docker.sock -v ${env.WORKSPACE}:${env.WORKSPACE} -w ${env.WORKSPACE} ${projectGitName} docker build -t ${projectGitName} ."
 }
