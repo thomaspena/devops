936da260aae679309b292ab7f9f604e16da11742
CristianBetancourthAvila <148814182+CristianBetancourthAvila@users.noreply.github.com>
2024-01-31 10:58:03 -0500

    Update lb_publicardockerhub.groovy

--- src/org/devops/lb_publicardockerhub.groovy
+++ src/org/devops/lb_publicardockerhub.groovy
@@ -1,16 +1,8 @@
 package org.devops
 
-/*def cargarDockerHub(projectGitName){
-    def dockerHubCredentials = credentials("${env.TOKEN_ID}")
-    sh "docker login -u ${dockerHubCredentials.username} -p ${dockerHubCredentials.password}"
-    sh "docker tag ${projectGitName} ${dockerHubCredentials.username}/${projectGitName}"
-    sh "docker push ${dockerHubCredentials.username}/${projectGitName}"
-}*/
-
 def cargarDockerHub(projectGitName){
-    //def dockerHubTokenCredentialId = "${env.TOKEN_ID}"
-    withCredentials([usernamePassword(credentialsId: "${env.TOKEN_ID}", passwordVariable: 'env.DOCKERHUB_PASSWORD', usernameVariable: 'env.DOCKERHUB_USERNAME')]) {
-        sh "docker login -u ${usernameVariable} -p ${passwordVariable}"
+    withCredentials([usernamePassword(credentialsId: "${env.TOKEN_ID}", passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
+        sh "docker login -u ${env.DOCKERHUB_USERNAME} -p ${env.DOCKERHUB_PASSWORD}"
         sh "docker tag ${projectGitName} ${env.DOCKERHUB_USERNAME}/${projectGitName}"
         sh "docker push ${env.DOCKERHUB_USERNAME}/${projectGitName}"
     }
