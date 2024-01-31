72543411c5f464a112d61444a35ad6cf93b2f48c
CristianBetancourthAvila <148814182+CristianBetancourthAvila@users.noreply.github.com>
2024-01-31 10:08:52 -0500

    Update lb_publicardockerhub.groovy

--- src/org/devops/lb_publicardockerhub.groovy
+++ src/org/devops/lb_publicardockerhub.groovy
@@ -1,8 +1,7 @@
 package org.devops
 
-def dockerHubCredentials = credentials(${env.TOKEN_ID})
-
 def cargarDockerHub(projectGitName){
+    def dockerHubCredentials = credentials(${env.TOKEN_ID})
     sh "docker login -u ${dockerHubCredentials.username} -p ${dockerHubCredentials.password}"
     sh "docker tag ${projectGitName} ${dockerHubCredentials.username}/${projectGitName}"
     sh "docker push ${dockerHubCredentials.username}/${projectGitName}"
