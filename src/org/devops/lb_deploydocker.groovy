0f5dfdabf4fc472cebd7fa105eec9e4c128e633e
CristianBetancourthAvila <148814182+CristianBetancourthAvila@users.noreply.github.com>
2024-02-01 09:30:51 -0500

    Update lb_deploydocker.groovy

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -1,7 +1,6 @@
 package org.devops
 
 def despliegueContenedor(projectGitName){
-    sh "docker network create ${env.NET_NAME}"
     sh "docker pull cristianbetancourthperiferia/react-test"
     sh "docker run -d --network=${env.NET_NAME} -p 5174:5174 --name ${projectGitName} cristianbetancourthperiferia/${projectGitName}"
 }
