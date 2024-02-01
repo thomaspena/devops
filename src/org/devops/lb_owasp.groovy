a23a68a1e392aa57e4bd2b9dfa132a8f040bb3da
CristianBetancourthAvila <148814182+CristianBetancourthAvila@users.noreply.github.com>
2024-02-01 14:37:55 -0500

    Update lb_owasp.groovy

--- src/org/devops/lb_owasp.groovy
+++ src/org/devops/lb_owasp.groovy
@@ -1,5 +1,5 @@
 package org.devops
 
 def analisisOWASP(){
-        sh "docker run --rm -v fullscan_react-test_OWASP:/zap/wrk/:rw --user root --network=${env.NET_NAME} -t owasp/zap2docker-stable zap-full-scan.py -t http://${env.IPHOST}:8081/ -r fullscan_react-test_OWASP.html -I"
+    sh "docker run --rm -v fullscan_react-test_OWASP:/zap/wrk/:rw --user root --network=${env.NET_NAME} -t owasp/zap2docker-stable zap-full-scan.py -t http://${env.IPHOST}:8081/ -r fullscan_react-test_OWASP.html -I"
 }
