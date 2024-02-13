c0c468e501afb60bb93781212f213150a8d2acd6
thomaspena <148784653+thomaspena@users.noreply.github.com>
2024-02-13 10:46:41 -0500

    Update lb_deploydocker.groovy

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -3,6 +3,6 @@ package org.devops
 def despliegueContenedor(projectGitName){
     sh "docker stop ${projectGitName}"
     sh "docker rm ${projectGitName}"
-    sh "docker pull cristianbetancourthperiferia/react-test"
-    sh "docker run -d --name ${projectGitName} --network=${env.NET_NAME} -p 5174:5174 --user root cristianbetancourthperiferia/${projectGitName}"
+    sh "docker pull thomaspena/react-test"
+    sh "docker run -d --name ${projectGitName} --network=${env.NET_NAME} -p 5174:5174 --user root thomaspena/${projectGitName}"
 }
