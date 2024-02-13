bb7fb58b1d212f74bd90292b3c79c79698a200b0
thomaspena <148784653+thomaspena@users.noreply.github.com>
2024-02-13 11:05:16 -0500

    Update lb_deploydocker.groovy

--- src/org/devops/lb_deploydocker.groovy
+++ src/org/devops/lb_deploydocker.groovy
@@ -2,7 +2,7 @@ package org.devops
 
 def despliegueContenedor(projectGitName){
     sh "docker stop festive_spence"
-    sh "docker rm ${projectGitName}"
+    sh "docker rm festive_spence"
     sh "docker pull thomaspena/festive_spence"
-    sh "docker run -d --name ${projectGitName} --network=${env.NET_NAME} -p 5174:5174 --user root thomaspena/${projectGitName}"
+    sh "docker run -d --name festive_spence --network=${env.NET_NAME} -p 5174:5174 --user root thomaspena/festive_spence"
 }
